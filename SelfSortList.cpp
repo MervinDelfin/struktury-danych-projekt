5fba7a85abce4a61bd52b53e8f00c335183c0f6a
Akip <89706618+Akip07@users.noreply.github.com>
2023-04-16 14:38:40 +0200

    commit

---
+++ SelfSortList.cpp
@@ -0,0 +1,117 @@
+#include "SelfSortList.h"
+
+
+SelfSortList::SelfSortList()
+{
+	head = nullptr;
+	rear = nullptr;
+}
+
+void SelfSortList::Insert(int num)
+{
+    
+    Snode* temp = new Snode;
+
+   
+    temp->val = num;
+    temp->count = 0;
+    temp->next = nullptr;
+
+    
+    if (head == nullptr)
+        head = rear = temp;
+
+ 
+    else {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+bool SelfSortList::Search(int key)
+{
+    Snode* current = head;
+
+
+    Snode* prev = nullptr;
+
+    while (current != nullptr) {
+
+        if (current->val == key) {
+
+            current->count = current->count + 1;
+
+  
+            if (current != head) {
+                Snode* temp = head;
+                Snode* temp_prev = nullptr;
+                while (current->count < temp->count) {
+                    temp_prev = temp;
+                    temp = temp->next;
+                }
+
+                if (current != temp) {
+                    prev->next = current->next;
+                    current->next = temp;
+
+                    if (temp == head)
+                        head = current;
+                    else
+                        temp_prev->next = current;
+                }
+            }
+            return 1;
+        }
+        prev = current;
+        current = current->next;
+    }
+    return 0;
+}
+
+void SelfSortList::Print()
+{
+    if (head == nullptr) {
+        std::cout << "List is empty" << std::endl;
+        return;
+    }
+
+    Snode* temp = head;
+    std::cout << "List: ";
+
+    while (temp != nullptr) {
+        std::cout << temp->val << "(" << temp->count << ")";
+        if (temp->next != nullptr)
+            std::cout << " --> ";
+
+        temp = temp->next;
+    }
+    std::cout << std::endl
+        << std::endl;
+}
+
+void SelfSortList::Remove(int key)
+{
+    Snode* current = head;
+
+    while (current-> next ->val != key) {
+
+        if (current->next == nullptr)
+            return;
+        else
+        {
+            current = current->next;
+        }
+        
+    }
+    Snode* todelete = current->next;
+    current->next = todelete->next;
+    delete[] todelete;
+}
+
+
+Snode::Snode()
+{
+    val = 0;
+    count = 0;
+    next = nullptr;
+}
