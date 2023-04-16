42dc546cd1ec2b78dea9cbb940337e4b4e35d150
Marvin Ruciński <marvin@prasa-polska.com>
2023-04-16 14:18:47 +0200

    added skip list implementation

---
+++ skip-list/skip-list.h
@@ -0,0 +1,36 @@
+#pragma once
+using namespace std;
+
+#ifndef SKIP_LIST_H
+#define SKIP_LIST_H
+
+class Node {
+public:
+    int key;
+
+    Node **forward;
+    Node(int, int); // key, level
+
+    friend ostream& operator<<(ostream& os, const Node& node);
+};
+
+
+class SkipList {
+    public:
+    int MAXLVL;
+    int level;
+    float P; // fraction of nodes compered to the previous level
+    Node *header;
+
+public:
+    SkipList(int, float); // MAXLVL, P
+    int randomLevel();
+    bool insertElement(int); // key
+    bool deleteElement(int); // key
+    void displayList();
+    Node* searchElement(int); // key
+    Node* nextNode(int); // key
+    Node* prevNode(int); // key
+};
+
+#endif
